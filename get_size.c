f04c217355a3795c6d8b6735cdda230e1e920fd2
Sylvester Henry <henrysylvester93@gmail.com>
2023-08-21 20:53:43 +0000

    Added get_size.c

---
+++ get_size.c
@@ -0,0 +1,31 @@
+/*
+ * Auth: Daniel Zadva and Henry Sylvester
+ */
+
+#include "main.h"
+
+/**
+ * get_size - This Calculates the size to cast the argument
+ * @format: The Formatted string in which to print the arguments
+ * @i: The List of arguments to be printed.
+ *
+ * Return: Precision.
+ */
+int get_size(const char *format, int *i)
+{
+	int curr_i = *i + 1;
+	int size = 0;
+
+	if (format[curr_i] == 'l')
+		size = S_LONG;
+	else if (format[curr_i] == 'h')
+		size = S_SHORT;
+
+	if (size == 0)
+		*i = curr_i - 1;
+	else
+		*i = curr_i;
+
+	return (size);
+}
+
